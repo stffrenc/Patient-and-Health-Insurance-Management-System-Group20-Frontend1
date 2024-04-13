65495b4525290d0bcfc0c2a1b2af6f117d10faed
French II, Stephan Floyd <stffrenc@iu.edu>
2024-04-13 13:13:20 -0400

    git fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Patient-and-Health-Insurance-Management-System-Group20-Frontend1/'
+  base: '/Patient-and-Health-Insurance-Management-System-Group20-Frontend1/',
   plugins: [react()],
 })
