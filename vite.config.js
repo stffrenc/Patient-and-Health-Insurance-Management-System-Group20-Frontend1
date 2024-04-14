18f7f35786791db7a091f3843ef7045a89243bb2
French II, Stephan Floyd <stffrenc@iu.edu>
2024-04-14 12:20:27 -0400

    config changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Patient-and-Health-Insurance-Management-System-Group20-Frontend1/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // This should match the directory name Render is expecting
+  }
 })
